e0b73bbf6039555b9a80f9eebe01d556a856bbca
tejtex <tejtex.dev@gmail.com>
2026-02-21 23:05:21 +0100

    feature/rayon-like-parallel-execution

--- src/lib.rs
+++ src/lib.rs
@@ -21,10 +21,35 @@
 //! let handle = MyTask(21).start();
 //! assert_eq!(handle.join().unwrap(), 42);
 //! ```
+//! 
+//! With `struct-threads`, you can also run multiple tasks in parallel using the [`ParallelRun`] extension trait.
+//! 
+//! # Example
+//! 
+//! ```rust
+//! use struct_threads::{Runnable, ParallelRun};
+//! 
+//! struct MyTask(i32);
+//! 
+//! impl Runnable for MyTask {
+//!     type Output = i32;
+//! 
+//!    fn run(self) -> Self::Output {
+//!        self.0 * 2
+//!    }
+//! }
+//! 
+//! let results = (0..10)
+//!     .map(MyTask)
+//!     .collect::<Vec<_>>()
+//!     .par_run();
+//! 
+//!  assert_eq!(results, (0..10).map(|x| x * 2).collect::<Vec<_>>());
+//! ```
 
 pub mod traits;
 
-pub use traits::{Runnable, Thread};
+pub use traits::{ParallelRun, Runnable, Thread};
 
 #[cfg(test)]
 mod tests {
@@ -44,5 +69,13 @@ mod tests {
         let task = TestTask(10);
         let handle = task.start();
         assert_eq!(handle.join().unwrap(), 20);
+    }
+
+    #[test]
+    fn par_test() {
+        let tasks = (0..100).map(TestTask).collect::<Vec<_>>();
+
+        let results = tasks.par_run();
+        assert_eq!(results, (0..100).map(|x| x * 2).collect::<Vec<_>>());
     }
 }
--- src/traits.rs
+++ src/traits.rs
@@ -1,3 +1,5 @@
+use std::sync::{Arc, Mutex};
+
 /// A trait for defining a task that can be executed, typically in a separate thread.
 ///
 /// Types implementing `Runnable` must be `Send` and `'static` to ensure they
@@ -72,5 +74,79 @@ pub trait Thread: Runnable {
 impl<T: Runnable> Thread for T {
     fn start(self) -> std::thread::JoinHandle<Self::Output> {
         std::thread::spawn(move || self.run())
+    }
+}
+
+/// An extension trait that provides a method to run multiple [`Runnable`]'s in parallel.
+///
+/// This trait is automatically implemented for any `Vec<T>` where `T` implements `Runnable`.
+/// You do not need to implement this trait manually.
+pub trait ParallelRun {
+    type Output: Send + 'static;
+
+    /// Spawns multiple threads to execute the `run` method of each task in parallel.
+    ///
+    /// The number of threads spawned will be equal to the number of available CPU cores.
+    ///
+    /// # Returns
+    ///
+    /// Returns a [`Vec<Self::Output>`] containing the results of each task, in the same order as the input vector.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use struct_threads::{Runnable, ParallelRun};
+    ///
+    /// struct MathTask(i32, i32);
+    ///
+    /// impl Runnable for MathTask {
+    ///    type Output = i32;
+    ///
+    ///   fn run(self) -> Self::Output {
+    ///       self.0 + self.1
+    ///   }
+    /// }
+    ///
+    /// let tasks = vec![MathTask(1, 2), MathTask(3, 4), MathTask(5, 6)];
+    ///
+    /// let results = tasks.par_run(); // Provided by the ParallelRun trait
+    /// assert_eq!(results, vec![3, 7, 11]);
+    /// ```
+    fn par_run(self) -> Vec<Self::Output>;
+}
+
+impl<T: Runnable> ParallelRun for Vec<T> {
+    type Output = T::Output;
+
+    fn par_run(self) -> Vec<Self::Output> {
+        let threads = std::thread::available_parallelism()
+            .map(|n| n.get())
+            .unwrap_or(1);
+
+        let queue = Arc::new(Mutex::new(self.into_iter()));
+        let handles = (0..threads).map(|i| {
+            let queue = Arc::clone(&queue);
+            std::thread::spawn(move || {
+                let mut results = Vec::new();
+                loop {
+                    let task = {
+                        let mut q = queue.lock().unwrap();
+                        q.next()
+                    };
+                    match task {
+                        Some(task) => results.push(task.run()),
+                        None => break,
+                    }
+                }
+                (results, i)
+            })
+        });
+
+        let mut results = handles
+            .into_iter()
+            .map(|h| h.join().unwrap())
+            .collect::<Vec<_>>();
+        results.sort_by_key(|&(_, i)| i);
+        results.into_iter().flat_map(|(res, _)| res).collect()
     }
 }
